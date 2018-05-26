aae80d291ab694b5803b41df49a366125e6d64b5
suliat16 <suliat16@gmail.com>
2018-05-26 21:32:23 +0300

    Added get_orthologs, a function which takes a lits of OrthoIDs, an index to determine which ID you want, and a file name to be given to the file that will be written, and returns a fasta file with all the sequences of the orthologs are inside. Note- edge cases (empty list, broken IDs) have not been thoroughly tested

--- cons.py
+++ cons.py
@@ -24,12 +24,21 @@ from Bio import SeqIO as sq
 import json
 import requests
 
-filename = "lysozyme.fasta"
 base_url = 'http://www.orthodb.org/'
 headers = {'Content-Type': 'application/json'}
     
-def get_response(file = ""):
+def get_cluster(file = ""):
     """
+    Take a fasta file and return a list of OrthoDB cluster IDs. Note that this 
+    is only useful for animal proteins.
+    
+    Args:
+        file (string): The name of the fasta file. Note that the fasta file must
+            be present in the same directory as the script
+    
+    Returns: 
+        A list of OrthoDB ortholog IDs which can then be used to retrieve a list 
+        of orthologs in a fasta file.
     """
     prerec = list(sq.parse(file,"fasta"))
     rec= prerec[0]
@@ -38,11 +47,45 @@ def get_response(file = ""):
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
-        return json.loads(response.content.decode('utf-8'))
+        response = json.loads(response.content.decode('utf-8'))
+        return response['data']
 
     else: 
         return None 
         # TODO: Wrap this in a try/catch block 
     
+def get_orthologs(IDlist= [], index=0, filename= 'default'):
+    """
+    Takes an OrthoDB ID, and returns a string consisting of sequences in fasta
+    format
+    
+    Args: 
+        IDlist (list): A list of strings, where each string is a OrthoDB cluster id
+        index(int): Specifies the index of the desired OrthoDB id, to retrieve the 
+            orthologs associated with that ID. Defaults to the first string in the
+            because the API returns a list with the best matching cluster first
+        filename (str): The name that will be given to the fasta file that is 
+            written.
+        
+    Returns: A fasta file containing the relevant orthologs in the same directory
+        as the program
+        
+    """
+    ID = IDlist[index]
+    url = '{0}/fasta?id={1}'.format(base_url, ID)
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        fast= response.content.decode('utf-8')
+        with open(filename+'.fasta', 'w') as workfile:
+            workfile.write(fast)
+    else:
+        return response.status_code
+    
 
-
+    
+    
+    
+    
+    
+    
