8ac8793bcfb97450bad997194f956c473dd7f453
suliat16 <suliat16@gmail.com>
2018-05-28 16:00:08 +0300

    Changed Docstring made get_orthoids take an absolute path as a variable, as opposed to the file itself- enables data to be stored in a separate directory I couldnt figure out what exception to raise in the event of a bad API query- I printed responses for the user to see instead

--- cons.py
+++ cons.py
@@ -1,64 +1,66 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#/usr/bin/env python3
+
 """
-Created on Thu May 24 10:50:45 2018
-
-@author: suliat16
-
-Hacky lil (script?) to get the conservation score of the residues in a protein
-
-Step 1: Open fasta file, and get the identifier- use this to make a call to the
-OrthoDB API. This should return a fasta file with the orthologs back
-
-Step 2: Enter this fasta file into the ClustalX wrapper inbuilt into Biopython
-- return an alignment object 
-
-Step 3: Get the information content for every position in the sequence, and input
-it in an array, with index corresponding with aa posn
-
-Step 4: Talk to a bioinformatician- convert the information content of position
-from the position
+    Intakes a protein sequence in fasta format, and returns the conservation 
+    score of the residues in the protein
 """
 
-##import os
+#import os
 from Bio import SeqIO as sq
 import json
 import requests
 from Bio.Align.Applications import ClustalwCommandline as cline
+from Bio import AlignIO as al
+from Bio.Align import AlignInfo as ai
+from Bio import Alphabet
 
 base_url = 'http://www.orthodb.org/'
 headers = {'Content-Type': 'application/json'}
     
-def get_orthoids(file = ""):
+def get_orthoids(filepath):
     """
     Take a fasta file and return a list of OrthoDB cluster IDs. Note that this 
     is only useful for animal proteins.
     
     Args:
-        file (string): The name of the fasta file. Note that the fasta file must
-            be present in the same directory as the script
-    
+        filepath (string): The absolute path leading to the fasta file of interest.
     Returns: 
         A list of OrthoDB ortholog IDs which can then be used to retrieve a list 
         of orthologs in a fasta file.
     """
-    prerec = list(sq.parse(file,"fasta"))
+    
+    with open(filepath) as workfile:
+        prerec = list(sq.parse(workfile, "fasta"))
     rec= prerec[0]
     st = rec.seq
     url = '{0}/blast?seq={1}&level=33208&limit=100'.format(base_url, st)
     # level in this case refers to the taxid from ncbi database- currently set 
     # at metazoa
     # limit 100 means that at most 100 OrthoDB IDs will be returned- not general
-    
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
         response = json.loads(response.content.decode('utf-8'))
         return response['data']
 
-    else: 
-        return None 
-        # TODO: Wrap this in a try/catch block 
+    elif response.status_code == 500:
+        print ('[!][{0}] Server Error'.format(response.status_code))
+        return None
+    elif response.status_code == 404:
+        print('[!] [{0}] URL not found: [{1}]'.format(response.status_code,url))
+        return None  
+    elif response.status_code == 401:
+        print('[!] [{0}] Authentication Failed'.format(response.status_code))
+        return None
+    elif response.status_code == 400:
+        print('[!] [{0}] Bad Request'.format(response.status_code))
+        return None
+    elif response.status_code == 300:
+        print('[!] [{0}] Unexpected Redirect'.format(response.status_code))
+        return None
+    else:
+        print('[?] Unexpected Error: [HTTP {0}]: Content: {1}'.format(response.status_code, response.content))
+    return None
     
 def get_orthologs(IDlist= [], index=0, filename= 'default'):
     """
@@ -104,10 +106,16 @@ def get_alignment(fastafile = "", path = r"/weyr/software/clustalw2/v2.1-bin.app
     stdout, stderr = clus_cline()  
     print(clus_cline)
 
-def get_information():
-    """
-    """
-    #Todo: Implement the information getting stuff
+#def get_information(filename = "", fileformat= "clustal"):
+#    """
+#    """
+#    prealign = list(al.parse(filename, fileformat))
+#    align = prealign[0]
+#    summary = ai.SummaryInfo(align)
+#    return summary.information_content()
+#    #info_content = summary.information_content()
+#    #return info_content
+    
     
     
     
