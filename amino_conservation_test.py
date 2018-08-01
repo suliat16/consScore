3d3cee1ca3af7d20213ed04d6074c77539494f99
suliat16 <suliat16@gmail.com>
2018-08-01 17:38:53 +0300

    T-coffee and get_alpha tests written

--- amino_conservation_test.py
+++ amino_conservation_test.py
@@ -5,7 +5,7 @@ Created on Sun Jul  1 16:58:25 2018
 
 @author: suliat16
 """
-
+import os
 import unittest
 import aminoCons as am
 
@@ -15,41 +15,61 @@ class test_amino_conservation(unittest.TestCase):
     """
 
     def setUp(self):
-        self.tester = am.AminoConservation()
+        self.filepath = os.getcwd() + os.sep + 'example_data' 
+        self.testscore = am.Rate4site(self.filepath +os.sep + 'multiFasta.aln')
+        self.rt2mat = self.testscore.run()
 
     def test_ortho_chk(self):
-        self.tester.ortholog_checker("Hello World")
-        self.assertEqual(self.tester.sequences, ">Input Sequence\nHello World")
+        output = am.ortholog_checker("Hello World")
+        self.assertEqual(output, ">Input Sequence\nHello World")
 
     def test_ortho_empty(self):
         with self.assertRaises(am.SequenceError) as cm:
-            self.tester.ortholog_checker("")
+            am.ortholog_checker("")
         err = cm.exception
         self.assertEqual(str(err), "Empty Sequence entered.")
 
     def test_non_sequence(self):
         with self.assertRaises(am.SequenceError) as sm:
-            self.tester.ortholog_checker("003893")
+            am.ortholog_checker("003893")
         err = sm.exception
         self.assertEqual(str(err), "Not a FASTA sequence. Please try again")
 
     def  test_ortho_already_fasta(self):
-        self.tester.ortholog_checker(">Pingo Pongo | Happiness\nWOEFJEKTJEJTEK")
-        self.assertEqual(self.tester.sequences, ">Pingo Pongo | Happiness\nWOEFJEKTJEJTEK")
+        output = am.ortholog_checker(">Pingo Pongo | Happiness\nWOEFJEKTJEJTEK")
+        self.assertEqual(output, ">Pingo Pongo | Happiness\nWOEFJEKTJEJTEK")
 
     def test_get_num_good(self):
-        digit = am.rate4site.get_num("Hello, how are all 1234.56 of you today")
+        digit = am.Rate4site.get_num("Hello, how are all 1234.56 of you today")
         self.assertEqual(digit[0], 1234.56)
 
     def test_get_num_multi(self):
-        digit = am.rate4site.get_num("Hello, how are all 1234.56 and 42 of you today")
+        digit = am.Rate4site.get_num("Hello, how are all 1234.56 and 42 of you today")
         self.assertEqual(digit, [1234.56, 42])
 
     def test_get_num_none(self):
-        digit = am.rate4site.get_num("Oh, there are none of you today")
+        digit = am.Rate4site.get_num("Oh, there are none of you today")
         self.assertEqual(digit, [])
+    
+    def test_tcof_output(self):
+        test = am.build_alignment(self.filepath + os.sep + 'multiFasta.fasta')
+        self.assertTrue(os.path.isdir(test))
+        with open(test + os.sep + 'multiFasta.fasta', 'r') as f:
+            contents = f.read()
+            self.assertIn('AACCGGTT', contents)
+
+    def test_get_alpha_g(self):
+        digit = self.testscore.get_alpha(self.filepath + os.sep + 'multiFasta.res')
+        self.assertEqual(digit, 2.83688)
+    
+    def test_get_alpha_b(self):
+        with self.assertRaises(am.Rate4SiteError) as cm:
+            self.testscore.get_alpha(self.filepath + os.sep + 'TheTree.txt')
+        err = cm.exception
+        self.assertEqual(str(err), 'File format is not supported')
+        
         
     #TODO: Test get_alpha
-    
+
 if __name__ == '__main__':
     unittest.main()
