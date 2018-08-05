8b6b16157b70aeb96f00f326c3dc210faf41b8b3
suliat16 <suliat16@gmail.com>
2018-08-05 13:46:51 +0300

    Tests for get_alpha written and implemented

--- amino_conservation_test.py
+++ amino_conservation_test.py
@@ -16,7 +16,7 @@ class test_amino_conservation(unittest.TestCase):
 
     def setUp(self):
         self.filepath = os.getcwd() + os.sep + 'example_data' 
-        self.testscore = am.Rate4site(self.filepath +os.sep + 'multiFasta.aln')
+        self.testscore = am.Rate4Site(self.filepath +os.sep + 'multiFasta.aln')
         self.rt2mat = self.testscore.run()
 
     def test_ortho_chk(self):
@@ -40,15 +40,15 @@ class test_amino_conservation(unittest.TestCase):
         self.assertEqual(output, ">Pingo Pongo | Happiness\nWOEFJEKTJEJTEK")
 
     def test_get_num_good(self):
-        digit = am.Rate4site.get_num("Hello, how are all 1234.56 of you today")
+        digit = am.Rate4Site.get_num("Hello, how are all 1234.56 of you today")
         self.assertEqual(digit[0], 1234.56)
 
     def test_get_num_multi(self):
-        digit = am.Rate4site.get_num("Hello, how are all 1234.56 and 42 of you today")
+        digit = am.Rate4Site.get_num("Hello, how are all 1234.56 and 42 of you today")
         self.assertEqual(digit, [1234.56, 42])
 
     def test_get_num_none(self):
-        digit = am.Rate4site.get_num("Oh, there are none of you today")
+        digit = am.Rate4Site.get_num("Oh, there are none of you today")
         self.assertEqual(digit, [])
     
     def test_tcof_output(self):
@@ -62,14 +62,17 @@ class test_amino_conservation(unittest.TestCase):
         digit = self.testscore.get_alpha(self.filepath + os.sep + 'multiFasta.res')
         self.assertEqual(digit, 2.83688)
     
-    def test_get_alpha_b(self):
-        with self.assertRaises(am.Rate4SiteError) as cm:
+    def test_get_alpha_nofile(self):
+        with self.assertRaises(FileNotFoundError) as cm:
             self.testscore.get_alpha(self.filepath + os.sep + 'TheTree.txt')
+
+    def test_getalpha_badfile(self):
+        with self.assertRaises(am.Rate4SiteError) as cm:
+            self.testscore.get_alpha(self.filepath + os.sep + 'Fak2Human.fasta')
         err = cm.exception
         self.assertEqual(str(err), 'File format is not supported')
-        
-        
-    #TODO: Test get_alpha
+
+
 
 if __name__ == '__main__':
     unittest.main()
