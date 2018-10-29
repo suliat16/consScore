25f1473ac4dcd33febeca0ba0cc2b7129b5f8597
suliat16 <34197105+suliat16@users.noreply.github.com>
2018-10-29 14:14:07 +0300

    Update __init__.py

--- consScore/__init__.py
+++ consScore/__init__.py
@@ -1,2 +1,1 @@
 ##ConsScore package
-name="example_pkg"
--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-VERSION='1.0dev'
+with open('README.md', 'r') as fh:
+      LONG_DESCRIPTION = fh.read()
+VERSION='1.0.0'
 
-setup(name='consScore',
+setuptools.setup(name='consScore',
       version=VERSION,
       author='Suliat Yakubu',
       author_email='suliat16@gmail.com',
       license='MIT',
       description='A set of methods combining programs to calculate the conservation score ' 
                   'of the amino acids in a sequence',
+      long_description = LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
       url= 'https://github.com/suliat16/consScore',
-      packages=['consScore'],
-      package_data={'example_data': []},
+      packages= setuptools.find_packages(),
+      include_package_data=True,
       classifiers=['License :: OSI Approved :: MIT License',
-                   'Programming Language :: Python 3'
-                   'Topic :: Scientific/Engineering :: Bio-Informatics'
-                   'Operating System :: POSIX :: Linux'
-                   'Intended Audience :: Science/Research'
-                   'Development Status :: 2 - Pre-Alpha']
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics',
+                   'Operating System :: POSIX :: Linux',
+                   'Intended Audience :: Science/Research',
+                   'Development Status :: 2 - Pre-Alpha'],
       )
